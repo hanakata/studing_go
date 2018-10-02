9ad9670545bcd096d336a5569521c5043e70f10d
katayama <katayama@localdomain>
2018-10-02 13:22:01 +0900

    1章完了

---
+++ プログラミング言語Go/第1章/server2/main.go
@@ -0,0 +1,33 @@
+// 最小限のechoとカウンタ機能を持つサーバ
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+//handlerはリクエストされたURLのpath要素を返す
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+//counterは呼び出し回数を返す
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
---
+++ プログラミング言語Go/第1章/練習問題/1.8/main.go
@@ -0,0 +1,33 @@
+//引数で指定されたURLに対しリクエストを発行し内容を表示する。
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	url := ""
+	for _, checkURL := range os.Args[1:] {
+		if !strings.HasPrefix(checkURL, "http://") {
+			url = "http://" + checkURL
+		} else {
+			url = checkURL
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
