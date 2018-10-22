16523943534f38005484729a440cad212b9e2a03
katayama <katayama@localdomain>
2018-10-22 14:49:15 +0900

    読了

---
+++ プログラミング言語Go/第11章/word2/word_test.go
@@ -0,0 +1,22 @@
+package word
+
+import "testing"
+
+func TestIsPalindrome(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"", true},
+		{"a", true},
+		{"aa", true},
+		{"ab", false},
+		{"kayak", true},
+		{"desserts", false},
+	}
+	for _, test := range tests {
+		if got := IsPalindrome(test.input); got != test.want {
+			t.Errorf("IsPlindrome(%q) = %v", test.input, got)
+		}
+	}
+}
